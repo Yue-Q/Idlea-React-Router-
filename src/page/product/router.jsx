f1d12285d450fc32b33c5dc50a77a57803ce1193
Yue Qiu <45703282+Yue-Q@users.noreply.github.com>
2020-09-17 09:34:38 -0400

    Update router.jsx

--- src/page/product/router.jsx
+++ src/page/product/router.jsx
@@ -1,9 +1,3 @@
-/*
-* @Author: Rosen
-* @Date:   2018-01-31 13:06:57
-* @Last Modified by:   Rosen
-* @Last Modified time: 2018-02-04 22:21:43
-*/
 import React            from 'react';
 import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom'
 
