753711f7f799d06ee31652e65da5f646c7bf5d4a
Yue Qiu <45703282+Yue-Q@users.noreply.github.com>
2020-09-17 09:39:17 -0400

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,3 @@
-/*
-* @Author: Rosen
-* @Date:   2018-01-13 11:27:21
-* @Last Modified by:   Rosen
-* @Last Modified time: 2018-02-05 14:02:20
-*/  
-
 import React            from 'react';
 import ReactDOM         from 'react-dom';
 import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom'
