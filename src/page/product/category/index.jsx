d02a3a5649eb7edbb1d9e036f27e3497290d958d
Yue Qiu <45703282+Yue-Q@users.noreply.github.com>
2020-09-22 18:14:06 -0400

    Update index.jsx

--- src/page/product/category/index.jsx
+++ src/page/product/category/index.jsx
@@ -1,9 +1,3 @@
-/*
-* @Author: Rosen
-* @Date:   2018-02-04 21:34:16
-* @Last Modified by:   Rosen
-* @Last Modified time: 2018-02-04 22:49:58
-*/
 import React        from 'react';
 import { Link }     from 'react-router-dom';
 import MUtil        from 'util/mm.jsx'
