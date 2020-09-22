6d62cad8dcea49dec193984aa112a8bdc6474c4a
Yue Qiu <45703282+Yue-Q@users.noreply.github.com>
2020-09-22 18:16:46 -0400

    Update jsx-base.jsx

--- gift/react/jsx-base.jsx
+++ gift/react/jsx-base.jsx
@@ -1,9 +1,3 @@
-/*
-* @Author: Rosen
-* @Date:   2018-01-19 17:40:57
-* @Last Modified by:   Rosen
-* @Last Modified time: 2018-01-19 17:52:40
-*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
