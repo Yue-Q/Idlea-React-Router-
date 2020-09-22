d53f94e45e319f0b421c505b8b376d6034d801b0
Yue Qiu <45703282+Yue-Q@users.noreply.github.com>
2020-09-22 18:19:09 -0400

    Update module.js

--- gift/es6-base/modules/module.js
+++ gift/es6-base/modules/module.js
@@ -1,9 +1,3 @@
-/*
-* @Author: mmall
-* @Date:   2018-01-06 20:40:26
-* @Last Modified by:   mmall
-* @Last Modified time: 2018-01-06 20:51:29
-*/
 let str = 'string';
 let obj = {
     name : 'Rosen'
